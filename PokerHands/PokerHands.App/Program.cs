bf31a2517c1a1036ff762a13cd14df056389b1bd
Vivek Chari <vivek.chari@xero.com>
2021-02-03 13:17:22 +1300

    Finishing up the console app

--- PokerHands/PokerHands.App/Program.cs
+++ PokerHands/PokerHands.App/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace PokerHands.App
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+using static PlayerExtensions;
+using static PokerHandRules;
+
+Console.WriteLine("Black:");
+var player1Cards = Console.ReadLine();
+
+Console.WriteLine("White:");
+var player2Cards = Console.ReadLine();
+
+var game = new PokerGame(new List<Player> {
+                new Player("Black", player1Cards.ToCards()),
+                new Player("White", player2Cards.ToCards())
+            })
+    .FindWinner(WithStraightFlush)
+    .FindWinner(WithFourOfAKind)
+    .FindWinner(WithAFullHouse)
+    .FindWinner(WithAFlush)
+    .FindWinner(WithStraight)
+    .FindWinner(WithThreeOfAKind)
+    .FindWinner(WithTwoPairs)
+    .FindWinner(WithAPair)
+    .FindWinner(WithHighCard);
+
+if (game.Winner != null)
+    Console.WriteLine($"{game.Winner.Name} wins with {game.WinningHand}");
+else
+    Console.WriteLine($"Tie");
---
+++ PokerHands/readme.md
@@ -0,0 +1,4 @@
+## Poker hands
+
+.NET core based solution for the kata: https://codingdojo.org/kata/PokerHands/
+
